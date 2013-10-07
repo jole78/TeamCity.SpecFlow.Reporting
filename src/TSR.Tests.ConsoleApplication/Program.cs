ca0d2cd336ab5803d9635c6737304f78ad6a39a2
Johan Leino <leino@outlook.com>
2013-10-07 11:11:36 +0200

    added test console app to test out-string (#10)

---
+++ src/TSR.Tests.ConsoleApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Options;
+
+namespace TSR.Tests.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string output = string.Empty;
+
+            var parameters = new OptionSet
+            {
+                {"output=", p => {output = p;}}
+            };
+
+            parameters.Parse(args);
+
+            Console.WriteLine(output);
+        }
+    }
+}
